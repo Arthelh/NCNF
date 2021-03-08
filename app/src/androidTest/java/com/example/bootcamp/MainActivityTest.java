8d845115563b6341631b87ef36607e37d6440128
Beltan <beltan.michel@epfl.ch>
2021-03-08 20:01:38 +0100

    Deleted useless test file

--- app/src/androidTest/java/com/example/bootcamp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/bootcamp/MainActivityTest.java
@@ -1,32 +1,21 @@
 package com.example.bootcamp;
 
-import android.content.Intent;
-import android.view.View;
-
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.bootcamp.map.MapActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.assertThat;
-
-import org.hamcrest.Matcher;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -34,14 +23,14 @@ public class MainActivityTest {
     public ActivityScenarioRule<MainActivity> testRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void first_test(){
+    public void test_intent(){
         Intents.init();
 
         String test_string = "This is a test";
         onView(withId(R.id.name_field)).perform(typeText(test_string), closeSoftKeyboard());
         onView(withId(R.id.name_button)).perform(click());
 
-        Intents.intended(hasComponent(DisplayMessageActivity.class.getName()));
+        Intents.intended(hasComponent(MapActivity.class.getName()));
         
 
         Intents.release();
