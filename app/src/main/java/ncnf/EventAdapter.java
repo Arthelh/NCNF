46988aee7cdbae84490ea9a469da419a15254c48
HyPn0TiiK <david.pedrucci@epfl.ch>
2021-03-08 13:48:21 +0100

    Basic FeedActivity working

--- app/src/main/java/ncnf/EventAdapter.java
+++ app/src/main/java/ncnf/EventAdapter.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.bootcamp.R;
+import com.ncnf.R;
 
 import java.util.LinkedList;
 import java.util.List;
--- app/src/main/java/ncnf/FeedActivity.java
+++ app/src/main/java/ncnf/FeedActivity.java
@@ -1,21 +1,47 @@
 package ncnf;
 
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
 
-public class FeedActivity extends WearableActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-    private TextView mTextView;
+import com.ncnf.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeedActivity extends AppCompatActivity {
+    private RecyclerView.LayoutManager lManager;
+    private EventAdapter adapter;
+    private static final String CHANNEL_NAME = "events_to_be_shown";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_feed);
         setContentView(R.layout.activity_feed);
+        findViewById(R.id.recycler_view);
+        // Get the RecyclerView
+        RecyclerView recycler = (RecyclerView) findViewById(R.id.recycler_view);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        // Use LinearLayout as the layout manager
+        lManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(lManager);
 
-        // Enables Always-on
-        setAmbientEnabled();
+        // Set the custom adapter
+        List<Event> eventList = new ArrayList<>();
+        eventList.add(new PublicEvent(new PublicOrganizer("testOrganizer"), EventType.Museum, "testName", "testData"));
+        adapter = new EventAdapter(eventList);
+        recycler.setAdapter(adapter);
+
+        EventListener eventListener = event -> runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Received event with data: " + event.getData());
+                adapter.addEvent(event);
+                ((LinearLayoutManager)lManager).scrollToPositionWithOffset(0, 0);
+            }
+        });
     }
 }
