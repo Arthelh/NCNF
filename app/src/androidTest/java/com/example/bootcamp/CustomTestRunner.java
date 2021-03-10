5300d3ff987fa6b892c6ad384c9499f6a3b5cb43
Keran Kocher <kocher.ke@gmail.com>
2021-03-10 09:38:54 +0100

    Map testing: switching between events and venue

---
+++ app/src/androidTest/java/com/example/bootcamp/CustomTestRunner.java
@@ -0,0 +1,18 @@
+package com.example.bootcamp;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.test.runner.AndroidJUnitRunner;
+
+import dagger.hilt.android.testing.HiltTestApplication;
+
+public class CustomTestRunner extends AndroidJUnitRunner {
+
+    @Override
+    public Application newApplication(ClassLoader cl, String className, Context context)
+            throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        return super.newApplication(cl, HiltTestApplication.class.getName(), context);
+    }
+
+}
---
+++ app/src/androidTest/java/com/example/bootcamp/map/MapActivityTest.java
@@ -0,0 +1,71 @@
+package com.example.bootcamp.map;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiSelector;
+
+import com.example.bootcamp.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import dagger.hilt.android.testing.BindValue;
+import dagger.hilt.android.testing.HiltAndroidRule;
+import dagger.hilt.android.testing.HiltAndroidTest;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertTrue;
+
+@HiltAndroidTest
+public final class MapActivityTest {
+
+    List<Event> TEST_EVENTS = Collections.singletonList(new Event("Math Conference, EPFL, 1pm March 3rd", 46.5191f, 6.5668f));
+    List<Venue> TEST_VENUES = Collections.singletonList(new Venue("EPFL", 46.5191f, 6.5668f));
+
+    private HiltAndroidRule hiltRule = new HiltAndroidRule(this);
+
+    @Rule
+    public RuleChain testRule = RuleChain.outerRule(hiltRule).around(new ActivityScenarioRule<>(MapActivity.class));
+
+    @BindValue
+    public EventProvider eventProvider = Mockito.mock(EventProvider.class);
+    @BindValue
+    public VenueProvider venueProvider = Mockito.mock(VenueProvider.class);
+
+    @Before
+    public void setup() {
+        Mockito.when(eventProvider.getAll()).thenReturn(TEST_EVENTS);
+        Mockito.when(venueProvider.getAll()).thenReturn(TEST_VENUES);
+
+        hiltRule.inject();
+    }
+
+    @Test
+    public final void switchMarkersUpdatesMap() {
+        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Events are shown
+        UiObject marker = device.findObject(new UiSelector().descriptionContains("Math"));
+        assertTrue(marker.exists());
+
+        onView(withId(R.id.map_switch_button)).perform(click());
+
+        // Venues are shown
+        marker = device.findObject(new UiSelector().descriptionContains("EPFL"));
+        assertTrue(marker.exists());
+
+        Mockito.verify(eventProvider).getAll();
+        Mockito.verify(venueProvider).getAll();
+    }
+}
