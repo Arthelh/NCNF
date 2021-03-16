06558155b1d8c1024c7325b5597d6572b0310c17
Jérémie Frei <36692804+Jnf14@users.noreply.github.com>
2021-03-16 18:11:45 +0100

    Update MainActivityTest.java

--- app/src/androidTest/java/com/ncnf/MainActivityTest.java
+++ app/src/androidTest/java/com/ncnf/MainActivityTest.java
@@ -3,9 +3,13 @@ package com.ncnf;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.ncnf.authentication.SignInActivity;
+import com.ncnf.authentication.SignUpActivity;
 import com.ncnf.main.MainActivity;
 import com.ncnf.map.MapActivity;
 import com.ncnf.feed.FeedActivity;
+import com.ncnf.user.UserProfileActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -44,6 +48,42 @@ public final class MainActivityTest {
 
         onView(withId(R.id.feed_view_button)).perform(click());
         Intents.intended(hasComponent(FeedActivity.class.getName()));
+
+        Intents.release();
+    }
+
+    @Test
+    public void signInTest(){
+        Intents.init();
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            onView(withId(R.id.mainSignInButton)).perform(click());
+            Intents.intended(hasComponent(SignInActivity.class.getName()));
+        }
+
+        Intents.release();
+    }
+
+    @Test
+    public void signUpTest(){
+        Intents.init();
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            onView(withId(R.id.mainSignUpButton)).perform(click());
+            Intents.intended(hasComponent(SignUpActivity.class.getName()));
+        }
+
+        Intents.release();
+    }
+
+    @Test
+    public void profileTest(){
+        Intents.init();
+
+        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+            onView(withId(R.id.mainProfileButton)).perform(click());
+            Intents.intended(hasComponent(UserProfileActivity.class.getName()));
+        }
 
         Intents.release();
     }
