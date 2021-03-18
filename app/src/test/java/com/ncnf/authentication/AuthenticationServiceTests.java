f9925842700733e1c919ffdb675b3a8c49bb1d2a
Jérémie Frei <jeremie.frei@epfl.ch>
2021-03-19 00:19:39 +0100

    Update AuthenticationServiceTests.java

--- app/src/test/java/com/ncnf/authentication/AuthenticationServiceTests.java
+++ app/src/test/java/com/ncnf/authentication/AuthenticationServiceTests.java
@@ -1,7 +1,14 @@
 package com.ncnf.authentication;
 
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -9,26 +16,27 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.util.concurrent.Executor;
+
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTests {
     private AuthResult authResult;
-    @Mock
-    private AuthenticationService mockedAuth;
-    private FirebaseAuth mockedFirebaseAuth;
-
-    MutableLiveData<Exception> nullException = null;
 
     @Before
     public void setup(){
-        mockedAuth = mock(AuthenticationService.class);
-        mockedFirebaseAuth = mock(FirebaseAuth.class);
+
     }
 
     @Test
     public void noExceptionOnRegisterTest(){
-        when(mockedAuth.register(anyString(), anyString())).thenReturn(nullException);
+
+    }
+
+    @Test
+    public void noExceptionOnSignInTest(){
+
     }
 }
