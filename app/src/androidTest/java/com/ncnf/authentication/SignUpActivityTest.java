cbf02a68550509adec95c08498ed2f21ab66a9b3
Jérémie Frei <jeremie.frei@epfl.ch>
2021-03-19 00:20:30 +0100

    Update SignUpActivityTest.java

--- app/src/androidTest/java/com/ncnf/authentication/SignUpActivityTest.java
+++ app/src/androidTest/java/com/ncnf/authentication/SignUpActivityTest.java
@@ -5,12 +5,17 @@ import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import com.ncnf.R;
+import com.ncnf.main.MainActivity;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
+
+import java.util.concurrent.CompletableFuture;
+
+import javax.inject.Inject;
 
 import dagger.hilt.android.testing.HiltAndroidRule;
 import dagger.hilt.android.testing.HiltAndroidTest;
@@ -20,6 +25,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.ncnf.Utils.BADLY_FORMATTED_EMAIL_STRING;
@@ -28,25 +34,33 @@ import static com.ncnf.Utils.EMPTY_STRING;
 import static com.ncnf.Utils.INVALID_PASSWORD_STRING;
 import static com.ncnf.Utils.PASSWORDS_DO_NOT_MATCH_STRING;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @HiltAndroidTest
 public class SignUpActivityTest {
+    private HiltAndroidRule hiltRule = new HiltAndroidRule(this);
+    private ActivityScenarioRule activityTestRule = new ActivityScenarioRule<>(SignUpActivity.class);
 
+    @Inject
+    AuthenticationService mockedAuth;
 
     String invalidEmail = "test";
     String validEmail = "test@test.com";
     String invalidPassword = "test";
     String validPassword = "test12";
-
-    private final HiltAndroidRule hiltRule = new HiltAndroidRule(this);
+    String unsuccessfulRegister = "Unsuccessful register.";
 
     @Rule
-    public RuleChain testRule = RuleChain.outerRule(hiltRule).around(new ActivityScenarioRule<>(SignUpActivity.class));
+    public RuleChain testRule = RuleChain.outerRule(hiltRule).around(activityTestRule);
 
     @Before
     public void setup(){
         Intents.init();
-//        hiltRule.inject();
+
+        mockedAuth = mock(AuthenticationService.class);
     }
 
     @After
@@ -94,4 +108,32 @@ public class SignUpActivityTest {
         onView(withId(R.id.signUpButton)).perform(click());
         onView(withId(R.id.signUpEmail)).check(matches(withText(EMPTY_STRING)));
     }
+
+    @Test
+    public void setProgressBarTest(){
+        onView(withId(R.id.progressBar)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.signUpEmail)).perform(typeText(validEmail), closeSoftKeyboard());
+        onView(withId(R.id.signUpPassword)).perform(typeText(validPassword), closeSoftKeyboard());
+        onView(withId(R.id.signUpConfirmPassword)).perform(typeText(validPassword), closeSoftKeyboard());
+        onView(withId(R.id.signUpButton)).perform(click());
+//        onView(withId(R.id.progressBar)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void unsuccessfulRegisterTest(){
+        Exception exception = new Exception(unsuccessfulRegister);
+        CompletableFuture<AuthenticationResponse> future = new CompletableFuture<>();
+        future.complete(new AuthenticationResponse(false, null, exception));
+
+        when(mockedAuth.register(anyString(), anyString())).thenReturn(future);
+
+        onView(withId(R.id.signUpEmail)).perform(typeText(validEmail), closeSoftKeyboard());
+        onView(withId(R.id.signUpPassword)).perform(typeText(validPassword), closeSoftKeyboard());
+        onView(withId(R.id.signUpConfirmPassword)).perform(typeText(validPassword), closeSoftKeyboard());
+        onView(withId(R.id.signUpButton)).perform(click());
+
+//        onView(withId(R.id.exceptionSignUp)).check(matches(withText(containsString(unsuccessfulRegister))));
+    }
+
 }
