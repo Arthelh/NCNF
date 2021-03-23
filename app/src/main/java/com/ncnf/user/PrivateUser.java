fa199086220f28dd44e95249b6906135a37e8b21
Eloi <eloigarandel@gmail.com>
2021-03-23 10:01:10 +0100

    new user and new profile

--- app/src/main/java/com/ncnf/user/PrivateUser.java
+++ app/src/main/java/com/ncnf/user/PrivateUser.java
@@ -1,7 +1,11 @@
 package com.ncnf.user;
 
+import android.app.VoiceInteractor;
+import android.os.Build;
+import android.provider.ContactsContract;
 import android.util.Log;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -9,23 +13,36 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirestoreRegistrar;
+import com.ncnf.database.DatabaseResponse;
+import com.ncnf.database.DatabaseService;
+import com.ncnf.event.Event;
+
+import org.xml.sax.SAXException;
+
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
 import static com.ncnf.Utils.*;
 
 public class PrivateUser {
 
     private static PrivateUser instance = new PrivateUser();
     private static FirebaseUser user;
-    private static DocumentReference docRef;
+    private static String path;
 
     public static PrivateUser getInstance(){
-        if(user == null || docRef == null){
+        if(user == null || path == null){
             instance = new PrivateUser();
         }
         return instance;
@@ -34,14 +51,22 @@ public class PrivateUser {
     private PrivateUser(){
         this.user = FirebaseAuth.getInstance().getCurrentUser();
         if(this.user != null){
-            this.docRef = FirebaseFirestore.getInstance().document(USERs_COLLECTION_KEY + user.getUid());
-            Log.d(DEBUG_TAG, this.docRef.toString());
+            this.path = USERs_COLLECTION_KEY + user.getUid();
         } else {
-            this.docRef = null;
+            this.path = null;
         }
     }
 
-    public void createDBUser(OnSuccessListener s, OnFailureListener f){
+    public String getID(){
+        if(this.user != null) {
+            return this.user.getUid();
+        }
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public CompletableFuture<DatabaseResponse> saveUserToDB(){
+        verifyCredential();
 
         Log.d(DEBUG_TAG, "Integrating new user to DB");
 
@@ -54,67 +79,68 @@ public class PrivateUser {
         initial_data.put(OWNED_EVENTS_KEY, new ArrayList<String>());
         initial_data.put(SAVED_EVENTS_KEY, new ArrayList<String>());
 
-        this.docRef.set(initial_data).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Log.d(DEBUG_TAG, "Successfully stored new user in DB");
-                    s.onSuccess(task.getResult());
-                } else {
-                    f.onFailure(task.getException());
-                    Log.d(DEBUG_TAG, "Error adding new user to DB : " + task.getException().getMessage());
-                }
-            }
-        });
+        return DatabaseService.getInstance().setDocument(this.path, initial_data);
     }
 
-    private void update(String key, Object value){
-        this.docRef.update(key, value).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
-                    Log.d(DEBUG_TAG, "Successfully updated the DB");
-                } else {
-                    Log.d(DEBUG_TAG, "Error updating DB : " + task.getException().getMessage());
-                }
-            }
-        });
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private CompletableFuture<DatabaseResponse> update(String key, Object value){
+        verifyCredential();
+        return DatabaseService.getInstance().updateField(path, key, value);
     }
 
-    public void updateLastName(String name){
-        this.update(LAST_NAME_KEY, name);
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public CompletableFuture<DatabaseResponse> getField(String field){
+        verifyCredential();
+        return DatabaseService.getInstance().getField(this.path, field);
     }
 
-    public void updateFirstName(String name){
-        this.update(FIRST_NAME_KEY, name);
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public CompletableFuture<DatabaseResponse>  updateLastName(String name){
+        return this.update(LAST_NAME_KEY, name);
     }
 
-    public void updateBirth(int year){
-        this.update(BIRTH_YEAR_KEY, year);
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public CompletableFuture<DatabaseResponse>  updateFirstName(String name){
+        return this.update(FIRST_NAME_KEY, name);
     }
 
-    public void loadUserFromBD(OnSuccessListener s, OnFailureListener f){
-        this.docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        s.onSuccess(document.getData());
-                    } else {
-                        f.onFailure(task.getException());
-                        Log.d(DEBUG_TAG, "No such document");
-                    }
-                } else {
-                    Log.d(DEBUG_TAG, "Failing retrieving data : ", task.getException());
-                }
-            }
-        });
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public CompletableFuture<DatabaseResponse>  updateBirth(int year){
+        return this.update(BIRTH_YEAR_KEY, year);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public CompletableFuture<DatabaseResponse> loadUserFromBD(){
+        verifyCredential();
+        return DatabaseService.getInstance().getData(this.path);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public CompletableFuture<DatabaseResponse> saveEvent(Event event){
+        return this.addEvent(event, SAVED_EVENTS_KEY);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public CompletableFuture<DatabaseResponse> ownEvent(Event event){
+        return this.addEvent(event, OWNED_EVENTS_KEY);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private CompletableFuture<DatabaseResponse> addEvent(Event event, String array){
+        verifyCredential();
+        return this.update(array, FieldValue.arrayUnion(event.getUID().toString()));
     }
 
     public void delete(){
         user = null;
-        docRef = null;
+        path = null;
+    }
+
+    public void verifyCredential(){
+        if(user == null || path == null){
+            throw new IllegalStateException("User doesn't have the right credentials to perform current operation");
+        }
     }
 
     @Override
@@ -123,7 +149,7 @@ public class PrivateUser {
         if (o == null || getClass() != o.getClass()) return false;
         PrivateUser that = (PrivateUser) o;
         if(this.user == null || that.user == null) return false;
-        return user.getUid().equals(that.user.getUid()) && this.docRef == that.docRef;
+        return user.getUid().equals(that.user.getUid()) && this.path == that.path;
     }
 
     @Override
--- app/src/main/java/com/ncnf/user/UserProfileActivity.java
+++ app/src/main/java/com/ncnf/user/UserProfileActivity.java
@@ -1,9 +1,13 @@
 package com.ncnf.user;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -11,13 +15,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.GeoPoint;
 import com.ncnf.R;
+import com.ncnf.database.DatabaseResponse;
+import com.ncnf.event.Event;
+import com.ncnf.event.EventProperty.*;
+import com.ncnf.event.PublicEvent;
+import com.ncnf.event.UserBookmark;
 import com.ncnf.main.MainActivity;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 import static com.ncnf.Utils.*;
 
@@ -31,7 +42,8 @@ public class UserProfileActivity extends AppCompatActivity {
     EditText firstName;
     EditText lastName;
     EditText birthDate;
-
+    private Intent intent;
+    private final Event event = new PublicEvent("name of event", new Date(), new GeoPoint(1.2, 2.1), "data", "1234", EventCategory.Conference, 0, 0, new ArrayList<>());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,40 +54,39 @@ public class UserProfileActivity extends AppCompatActivity {
         lastName = findViewById(R.id.userProfileLastName);
         birthDate = findViewById(R.id.userProfileDateOfBirth);
         this.prepareFields();
+        this.intent = new Intent(this, UserBookmark.class);
+        findViewById(R.id.userProfileProgresssBar).setVisibility(View.INVISIBLE);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onStart() {
         super.onStart();
         if(FirebaseAuth.getInstance().getCurrentUser() != null){
             user = PrivateUser.getInstance();
-            user.loadUserFromBD(new OnSuccessListener() {
-                @Override
-                public void onSuccess(Object o) {
-                    Map<String, Object> map = (Map<String, Object>) o;
-//                    String first_name = map.getOrDefault(FIRST_NAME_KEY, EMPTY_STRING).toString();
-//                    String last_name = map.getOrDefault(LAST_NAME_KEY, EMPTY_STRING).toString();
-//                    String birth_date = map.getOrDefault(BIRTH_YEAR_KEY, EMPTY_STRING).toString();
+            user.loadUserFromBD().thenAccept(task -> {
 
-                    String first_name = "test";
-                    String last_name = "test";
-                    String birth_date = "1234";
+                if(task.isSuccessful()) {
+                    Map<String, Object> map = (Map<String, Object>) task.getResult();
+                    String first_name = map.get(FIRST_NAME_KEY).toString();
+                    String last_name = map.get(LAST_NAME_KEY).toString();
+                    String birth_date = map.get(BIRTH_YEAR_KEY).toString();
 
                     firstName.setText(first_name);
                     lastName.setText(last_name);
                     birthDate.setText(birth_date);
                     findViewById(R.id.userProfileSaveButton).setEnabled(false);
-
                     email.setEnabled(false);
                     email.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
-                }
-            }, new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Log.d(DEBUG_TAG, "Unable to load user from db : " + e.getMessage());
+                } else {
+                    Log.d(DEBUG_TAG, "Unable to load user from db : " + task.getException().getMessage());
                 }
             });
+
+            this.event.setOwner(PrivateUser.getInstance().getID());
+            event.storeEventInDB();
         }
+
     }
 
     public void logOut(View view) {
@@ -137,22 +148,84 @@ public class UserProfileActivity extends AppCompatActivity {
         });
     }
 
+    private void resetBar(){
+        if(!firstNameChanged && !lastNameChanged && !birthDateChanged){
+            findViewById(R.id.userProfileProgresssBar).setVisibility(View.INVISIBLE);
+        }
+    }
+
+    ///TODO: find a way to refactor -> save multiple fields at the time but how ?
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void saveNewFields(View view){
+        findViewById(R.id.userProfileSaveButton).setEnabled(false);
 
         if(firstNameChanged){
-            this.user.updateFirstName(firstName.getText().toString());
-            firstNameChanged = false;
+            CompletableFuture<DatabaseResponse> changed = this.user.updateFirstName(firstName.getText().toString());
+            changed.thenAccept(task -> {
+                if(task.isSuccessful()) {
+                    firstNameChanged = false;
+                } else {
+                    findViewById(R.id.userProfileSaveButton).setEnabled(true);
+                }
+                resetBar();
+            });
         }
 
         if(lastNameChanged){
-            this.user.updateLastName(lastName.getText().toString());
-            lastNameChanged = false;
+            CompletableFuture<DatabaseResponse> changed = this.user.updateLastName(lastName.getText().toString());
+            changed.thenAccept(task -> {
+                if(task.isSuccessful()){
+                    lastNameChanged = false;
+                } else {
+                    findViewById(R.id.userProfileSaveButton).setEnabled(true);
+                }
+                resetBar();
+            });
         }
         if(birthDateChanged){
-            this.user.updateBirth(Integer.parseInt(birthDate.getText().toString()));
-            birthDateChanged = false;
+            CompletableFuture<DatabaseResponse> changed = this.user.updateBirth(Integer.parseInt(birthDate.getText().toString()));
+            changed.thenAccept(task -> {
+                if(task.isSuccessful()){
+                    birthDateChanged = false;
+                } else {
+                    findViewById(R.id.userProfileSaveButton).setEnabled(true);
+                }
+                resetBar();
+            });
 
         }
-        findViewById(R.id.userProfileSaveButton).setEnabled(false);
+    }
+
+    public void viewBookmark(View view){
+        startActivity(intent);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void saveEvent(View view){
+        findViewById(R.id.userProfileProgresssBar).setVisibility(View.VISIBLE);
+
+        CompletableFuture<DatabaseResponse> response  = PrivateUser.getInstance().saveEvent(event);
+        response.thenAccept(task -> {
+            findViewById(R.id.userProfileProgresssBar).setVisibility(View.INVISIBLE);
+
+            String alertMessage;
+           if(task.isSuccessful()){
+                alertMessage = "Event has been successfully saved in your bookmark";
+           } else {
+               alertMessage = "Couldn't save this event : please retry";
+           }
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(UserProfileActivity.this);
+           builder.setCancelable(false);
+           builder.setTitle("Saving event");
+           builder.setMessage(alertMessage);
+           builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+               @Override
+               public void onClick(DialogInterface dialog, int which) {
+                   return;
+               }
+           });
+           builder.show();
+        });
     }
 }
