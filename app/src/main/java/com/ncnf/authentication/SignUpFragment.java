a5accd8410a67e332e21e780a43e9ed95db53e13
Jérémie Frei <jeremie.frei@epfl.ch>
2021-03-24 17:28:34 +0100

    Update SignUpFragment.java

--- app/src/main/java/com/ncnf/authentication/SignUpFragment.java
+++ app/src/main/java/com/ncnf/authentication/SignUpFragment.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -44,6 +45,15 @@ public class SignUpFragment extends Fragment {
     @Inject
     AuthenticationService auth;
 
+    private Boolean isOrganizer = false;
+
+    private EditText email;
+    private EditText password;
+    private EditText confirmPassword;
+    private TextView exceptionText;
+    private TextView organizerText;
+    private Button registerButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -55,40 +65,34 @@ public class SignUpFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        setProgressBar(View.INVISIBLE);
-        setFieldsEmpty();
+        // Get all items from view
+        getItemsFromView();
 
-        getView().findViewById(R.id.signUpButton).setOnClickListener(v -> signUp());
+        showProgressBar(false);
+
+        registerButton.setOnClickListener(v -> signUp());
+        organizerText.setOnClickListener(v -> {
+            if(isOrganizer){
+                setPrivateUserView();
+            } else {
+                setOrganizerView();
+            }
+        });
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void signUp() {
-        String email = getFieldText(R.id.signUpEmail);
-        String password = getFieldText(R.id.signUpPassword);
-        String confirmed = getFieldText(R.id.signUpConfirmPassword);
+        String emailString = this.email.getText().toString();
+        String passwordString = this.password.getText().toString();
 
-        setFieldsEmpty();
-
-        if(email.isEmpty() || password.isEmpty() || confirmed.isEmpty()){
-            setException(EMPTY_FIELD_STRING);
-            return;
-        } else if(!password.equals(confirmed)){
-            setException(PASSWORDS_DO_NOT_MATCH_STRING);
-            return;
-        } else if (!isValidEmail(email)){
-            setException(BADLY_FORMATTED_EMAIL_STRING);
-            return;
-        } else if (!isValidPassword(password)){
-            setException(INVALID_PASSWORD_STRING);
-            return;
+        if(checkInputs()){
+            register(emailString, passwordString);
         }
-
-        register(email, password);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void register(String email, String password){
-        setProgressBar(View.VISIBLE);
+        showProgressBar(true);
 
         CompletableFuture<AuthenticationResponse> futureResponse= auth.register(email, password);
 
@@ -111,30 +115,77 @@ public class SignUpFragment extends Fragment {
                 Log.d(DEBUG_TAG,"Unsuccessful register for " + email + " : " + response.getException().getMessage());
                 setException(response.getException().getMessage());
             }
-            setProgressBar(View.INVISIBLE);
+            showProgressBar(false);
         });
     }
 
-    private void setFieldsEmpty(){
-        ((EditText)getView().findViewById(R.id.signUpEmail)).setText(EMPTY_STRING);
-        ((EditText)getView().findViewById(R.id.signUpPassword)).setText(EMPTY_STRING);
-        ((EditText)getView().findViewById(R.id.signUpConfirmPassword)).setText(EMPTY_STRING);
+    private void getItemsFromView(){
+        this.email = getView().findViewById(R.id.signUpEmail);
+        this.password = getView().findViewById(R.id.signUpPassword);
+        this.confirmPassword = getView().findViewById(R.id.signUpConfirmPassword);
+        this.exceptionText = getView().findViewById(R.id.exceptionSignUp);
+        this.organizerText = getView().findViewById(R.id.signUpOrganizerText);
+        this.registerButton = getView().findViewById(R.id.signUpRegisterButton);
     }
 
-    private String getFieldText(int id){
-        return ((EditText)getView().findViewById(id)).getText().toString();
+    private boolean checkInputs(){
+        String emailString = this.email.getText().toString();
+        String passwordString = this.password.getText().toString();
+        String confirmPasswordString = this.confirmPassword.getText().toString();
+
+        if(emailString.isEmpty() || passwordString.isEmpty() || confirmPasswordString.isEmpty()){
+            setException(EMPTY_FIELD_STRING);
+            return false;
+        } else if(!passwordString.equals(confirmPasswordString)){
+            setException(PASSWORDS_DO_NOT_MATCH_STRING);
+            confirmPassword.setError(PASSWORDS_DO_NOT_MATCH_STRING);
+            password.setText(EMPTY_STRING);
+            confirmPassword.setText(EMPTY_STRING);
+            return false;
+        } else if (!isValidEmail(emailString)){
+            setException(BADLY_FORMATTED_EMAIL_STRING);
+            email.setError(BADLY_FORMATTED_EMAIL_STRING);
+            return false;
+        } else if (!isValidPassword(passwordString)){
+            setException(INVALID_PASSWORD_STRING);
+            password.setError(INVALID_PASSWORD_STRING);
+            password.setText(EMPTY_STRING);
+            confirmPassword.setText(EMPTY_STRING);
+            return false;
+        }
+
+        return true;
     }
 
-    private void setProgressBar(int visibility){
+    private void showProgressBar(Boolean show){
         ProgressBar bar = getView().findViewById(R.id.progressBar);
-        bar.setVisibility(visibility);
+        if(show){
+            bar.setVisibility(View.VISIBLE);
+        } else {
+            bar.setVisibility(View.INVISIBLE);
+        }
     }
 
     private void setException(String s){
-        TextView exception = getView().findViewById(R.id.exceptionSignUp);
-        exception.setText(s);
+        exceptionText.setText(s);
         Toast.makeText(getActivity(), s, Toast.LENGTH_LONG).show();
     }
+
+    private void setOrganizerView(){
+        isOrganizer = true;
+        setException("Organizer Registration");
+        email.setHint("Business Email..");
+        organizerText.setText("I'm a regular user...");
+    }
+
+    private void setPrivateUserView(){
+        isOrganizer = false;
+        setException("Register");
+        email.setHint("Email..");
+        organizerText.setText("I'm an organizer...");
+    }
+
+
 
 
 }
