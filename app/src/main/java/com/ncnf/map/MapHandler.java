beb72e5293de80a4a8f9cb82b5463144c62e9b46
Jérémie Frei <jeremie.frei@epfl.ch>
2021-04-01 13:49:15 +0200

    Update MapHandler.java

--- app/src/main/java/com/ncnf/map/MapHandler.java
+++ app/src/main/java/com/ncnf/map/MapHandler.java
@@ -40,7 +40,7 @@ public class MapHandler {
     private LatLng userPosition;
     private Marker userMarker;
     private ArrayList<Marker> eventMarkers, venueMarkers;
-    private List<AutocompletePrediction> predictionList;
+//    private List<AutocompletePrediction> predictionList;
 
     // Indicate whether Events or Venues are shown. If false -> venues are shown
     private boolean eventsShown = true;
@@ -55,131 +55,131 @@ public class MapHandler {
         userPosition = new LatLng(46.526120f, 6.576330f);
         eventMarkers = new ArrayList<>();
         venueMarkers = new ArrayList<>();
-        predictionList = new ArrayList<>();
+//        predictionList = new ArrayList<>();
     }
 
-    public void createOnSearchActionListener(MaterialSearchBar materialSearchBar){
-        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
-            @Override
-            public void onSearchStateChanged(boolean enabled) {
-
-            }
-
-            @Override
-            public void onSearchConfirmed(CharSequence text) {
-                context.startSearch(text.toString(), true, null, true);
-            }
-
-            @Override
-            public void onButtonClicked(int buttonCode) {
-                if (buttonCode == MaterialSearchBar.BUTTON_NAVIGATION){
-                    //Define behaviour for navigation button
-                } else if (buttonCode == MaterialSearchBar.BUTTON_BACK){
-                    materialSearchBar.closeSearch();
-                }
-            }
-        });
-    }
-
-    //Sets the behavior for when text is changed in the search bar
-    public void createTextChangeListener(MaterialSearchBar materialSearchBar, AutocompleteSessionToken token, PlacesClient placesClient){
-        materialSearchBar.addTextChangeListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                //Creates the prediction auto completer, accepts Cities, Addresses
-                FindAutocompletePredictionsRequest predictionsRequest = FindAutocompletePredictionsRequest.builder()
-                        .setCountries(Utilities.supported_countries)
-                        .setSessionToken(token)
-                        .setQuery(s.toString()).build();
-                placesClient.findAutocompletePredictions(predictionsRequest).addOnCompleteListener(task -> {
-                    if (task.isSuccessful()){
-                        FindAutocompletePredictionsResponse predictionsResponse = task.getResult();
-                        if (predictionsResponse != null){
-                            //Retrieves the predictions and adds them to the list to be displayed below the search bar
-                            predictionList.clear();
-                            predictionList.addAll(predictionsResponse.getAutocompletePredictions());
-                            List<String> suggestions = new ArrayList<>();
-                            for (AutocompletePrediction a : predictionList){
-                                suggestions.add(a.getFullText(null).toString());
-                            }
-                            materialSearchBar.updateLastSuggestions(suggestions);
-                            if (!materialSearchBar.isSuggestionsVisible()){
-                                materialSearchBar.showSuggestionsList();
-                            }
-                        }
-                    } else {
-                        FindAutocompletePredictionsResponse predictionsResponse = task.getResult();
-                        Log.i("AutoCompleteTask", "Prediction fetching task unsuccessful, status is: " + predictionsResponse.toString());
-                    }
-                });
-
-                //This decides what to do once the user clicked on one of the given suggestions
-                createSuggestionsClickListener(materialSearchBar, placesClient);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-    }
-
-    //Creates the behavior for when clicking on a suggestion
-    private void createSuggestionsClickListener(MaterialSearchBar materialSearchBar, PlacesClient placesClient){
-        materialSearchBar.setSuggestionsClickListener(new SuggestionsAdapter.OnItemViewClickListener() {
-            @Override
-            public void OnItemClickListener(int position, View v) {
-                if (position >= predictionList.size())
-                    return;
-                AutocompletePrediction selection = predictionList.get(position);
-                //Stores the address as a human-readable String in 'suggestion'
-                String suggestion = materialSearchBar.getLastSuggestions().get(position).toString();
-                materialSearchBar.setText(suggestion);
-
-                //Because apparently just putting mSB.clearSuggestions() does not work
-                new Handler().postDelayed(materialSearchBar::clearSuggestions, 1000);
-
-                //Hides the keyboard
-                InputMethodManager input = (InputMethodManager) context.getSystemService(INPUT_METHOD_SERVICE);
-                if (input != null)
-                    input.hideSoftInputFromWindow(materialSearchBar.getWindowToken(), InputMethodManager.HIDE_IMPLICIT_ONLY);
-
-                //Prepares the conversion from prediction to LatLng
-                final String placeId = selection.getPlaceId();
-                List<Place.Field> placeFields = Arrays.asList(Place.Field.LAT_LNG);
-
-                //Converts prediction to LatLng and updates markers if successful, prints out errors if not
-                FetchPlaceRequest fetchPlaceRequest = FetchPlaceRequest.builder(placeId, placeFields).build();
-                placesClient.fetchPlace(fetchPlaceRequest).addOnSuccessListener(fetchPlaceResponse -> {
-                    Place place = fetchPlaceResponse.getPlace();
-                    Log.i("Place fetching", "Place found: " + place.getName());
-                    LatLng placeLatLng = place.getLatLng();
-                    if (placeLatLng != null) {
-                        userPosition = placeLatLng;
-                        update_markers();
-                    }
-                }).addOnFailureListener(e -> {
-                    if (e instanceof ApiException){
-                        ApiException apiException = (ApiException) e;
-                        apiException.printStackTrace();
-                        int statusCode = apiException.getStatusCode();
-                        Log.i("Place fetching", "Place not found: " + e.getMessage());
-                        Log.i("Place fetching", "Status code: " + statusCode);
-                    }
-                });
-            }
-
-            @Override
-            public void OnItemDeleteListener(int position, View v) {
-
-            }
-        });
-    }
+//    public void createOnSearchActionListener(MaterialSearchBar materialSearchBar){
+//        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+//            @Override
+//            public void onSearchStateChanged(boolean enabled) {
+//
+//            }
+//
+//            @Override
+//            public void onSearchConfirmed(CharSequence text) {
+//                context.startSearch(text.toString(), true, null, true);
+//            }
+//
+//            @Override
+//            public void onButtonClicked(int buttonCode) {
+//                if (buttonCode == MaterialSearchBar.BUTTON_NAVIGATION){
+//                    //Define behaviour for navigation button
+//                } else if (buttonCode == MaterialSearchBar.BUTTON_BACK){
+//                    materialSearchBar.closeSearch();
+//                }
+//            }
+//        });
+//    }
+//
+//    //Sets the behavior for when text is changed in the search bar
+//    public void createTextChangeListener(MaterialSearchBar materialSearchBar, AutocompleteSessionToken token, PlacesClient placesClient){
+//        materialSearchBar.addTextChangeListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                //Creates the prediction auto completer, accepts Cities, Addresses
+//                FindAutocompletePredictionsRequest predictionsRequest = FindAutocompletePredictionsRequest.builder()
+//                        .setCountries(Utilities.supported_countries)
+//                        .setSessionToken(token)
+//                        .setQuery(s.toString()).build();
+//                placesClient.findAutocompletePredictions(predictionsRequest).addOnCompleteListener(task -> {
+//                    if (task.isSuccessful()){
+//                        FindAutocompletePredictionsResponse predictionsResponse = task.getResult();
+//                        if (predictionsResponse != null){
+//                            //Retrieves the predictions and adds them to the list to be displayed below the search bar
+//                            predictionList.clear();
+//                            predictionList.addAll(predictionsResponse.getAutocompletePredictions());
+//                            List<String> suggestions = new ArrayList<>();
+//                            for (AutocompletePrediction a : predictionList){
+//                                suggestions.add(a.getFullText(null).toString());
+//                            }
+//                            materialSearchBar.updateLastSuggestions(suggestions);
+//                            if (!materialSearchBar.isSuggestionsVisible()){
+//                                materialSearchBar.showSuggestionsList();
+//                            }
+//                        }
+//                    } else {
+//                        FindAutocompletePredictionsResponse predictionsResponse = task.getResult();
+//                        Log.i("AutoCompleteTask", "Prediction fetching task unsuccessful, status is: " + predictionsResponse.toString());
+//                    }
+//                });
+//
+//                //This decides what to do once the user clicked on one of the given suggestions
+//                createSuggestionsClickListener(materialSearchBar, placesClient);
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//            }
+//        });
+//    }
+//
+//    //Creates the behavior for when clicking on a suggestion
+//    private void createSuggestionsClickListener(MaterialSearchBar materialSearchBar, PlacesClient placesClient){
+//        materialSearchBar.setSuggestionsClickListener(new SuggestionsAdapter.OnItemViewClickListener() {
+//            @Override
+//            public void OnItemClickListener(int position, View v) {
+//                if (position >= predictionList.size())
+//                    return;
+//                AutocompletePrediction selection = predictionList.get(position);
+//                //Stores the address as a human-readable String in 'suggestion'
+//                String suggestion = materialSearchBar.getLastSuggestions().get(position).toString();
+//                materialSearchBar.setText(suggestion);
+//
+//                //Because apparently just putting mSB.clearSuggestions() does not work
+//                new Handler().postDelayed(materialSearchBar::clearSuggestions, 1000);
+//
+//                //Hides the keyboard
+//                InputMethodManager input = (InputMethodManager) context.getSystemService(INPUT_METHOD_SERVICE);
+//                if (input != null)
+//                    input.hideSoftInputFromWindow(materialSearchBar.getWindowToken(), InputMethodManager.HIDE_IMPLICIT_ONLY);
+//
+//                //Prepares the conversion from prediction to LatLng
+//                final String placeId = selection.getPlaceId();
+//                List<Place.Field> placeFields = Arrays.asList(Place.Field.LAT_LNG);
+//
+//                //Converts prediction to LatLng and updates markers if successful, prints out errors if not
+//                FetchPlaceRequest fetchPlaceRequest = FetchPlaceRequest.builder(placeId, placeFields).build();
+//                placesClient.fetchPlace(fetchPlaceRequest).addOnSuccessListener(fetchPlaceResponse -> {
+//                    Place place = fetchPlaceResponse.getPlace();
+//                    Log.i("Place fetching", "Place found: " + place.getName());
+//                    LatLng placeLatLng = place.getLatLng();
+//                    if (placeLatLng != null) {
+//                        userPosition = placeLatLng;
+//                        update_markers();
+//                    }
+//                }).addOnFailureListener(e -> {
+//                    if (e instanceof ApiException){
+//                        ApiException apiException = (ApiException) e;
+//                        apiException.printStackTrace();
+//                        int statusCode = apiException.getStatusCode();
+//                        Log.i("Place fetching", "Place not found: " + e.getMessage());
+//                        Log.i("Place fetching", "Status code: " + statusCode);
+//                    }
+//                });
+//            }
+//
+//            @Override
+//            public void OnItemDeleteListener(int position, View v) {
+//
+//            }
+//        });
+//    }
 
     public void switchMarkers(GoogleMap mMap) {
         eventsShown = !eventsShown;
@@ -241,4 +241,7 @@ public class MapHandler {
         show_markers(mMap, eventProvider, venueProvider);
     }
 
+    public void setUserPosition(LatLng userPosition) {
+        this.userPosition = userPosition;
+    }
 }
