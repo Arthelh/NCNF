7620597b29e6925cbcddb23c5223af72fbeb0b45
Jérémie Frei <jeremie.frei@epfl.ch>
2021-04-06 19:12:57 +0200

    Update MainActivity.java

--- app/src/main/java/com/ncnf/main/MainActivity.java
+++ app/src/main/java/com/ncnf/main/MainActivity.java
@@ -1,13 +1,10 @@
 package com.ncnf.main;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ncnf.R;
