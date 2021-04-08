a50652c46e243514bcd7462c5249dfe239055318
Jérémie Frei <jeremie.frei@epfl.ch>
2021-04-08 21:04:07 +0200

    Update DatabaseService implementation to increase coverage

--- app/src/main/java/com/ncnf/database/DatabaseService.java
+++ app/src/main/java/com/ncnf/database/DatabaseService.java
@@ -1,46 +1,44 @@
 package com.ncnf.database;
 
-import android.os.Build;
-import android.util.Log;
+        import android.os.Build;
+        import android.util.Log;
 
-import androidx.annotation.RequiresApi;
+        import androidx.annotation.RequiresApi;
 
-import com.google.android.gms.tasks.SuccessContinuation;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.FieldPath;
-import com.google.firebase.firestore.FieldValue;
-import com.google.firebase.firestore.FirebaseFirestore;
+        import com.google.android.gms.tasks.SuccessContinuation;
+        import com.google.android.gms.tasks.Task;
+        import com.google.firebase.auth.AuthResult;
+        import com.google.firebase.firestore.DocumentSnapshot;
+        import com.google.firebase.firestore.FieldPath;
+        import com.google.firebase.firestore.FieldValue;
+        import com.google.firebase.firestore.FirebaseFirestore;
+        import com.ncnf.authentication.AuthenticationResponse;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.FutureTask;
+        import java.util.ArrayList;
+        import java.util.Map;
+        import java.util.concurrent.CompletableFuture;
+        import java.util.concurrent.FutureTask;
 
-import static com.ncnf.Utils.*;
+        import static com.ncnf.Utils.*;
 
 public class DatabaseService implements DatabaseServiceInterface {
 
     private final FirebaseFirestore db;
-   
+
     public DatabaseService(){
         this.db = FirebaseFirestore.getInstance();
     }
 
-    public DatabaseService(FirebaseFirestore db)  {
+    protected DatabaseService(FirebaseFirestore db)  {
         this.db = db;
     }
 
     @Override
     public CompletableFuture<DatabaseResponse> setDocument(String path, Map<String, Object> fields) {
         CompletableFuture<DatabaseResponse> futureResponse = new CompletableFuture<>();
-        this.db.document(path).set(fields).addOnCompleteListener(task -> {
-           try {
-               futureResponse.complete(new DatabaseResponse(task.isSuccessful(), task.getResult(), task.getException()));
-           } catch (Exception e){
-               Log.d(DEBUG_TAG, "Unable to set document from DB", task.getException());
-               futureResponse.complete(new DatabaseResponse(false, null, e));
-           }
-        });
+
+        this.db.document(path).set(fields).addOnCompleteListener(task -> onTaskComplete(task, futureResponse));
+
         return futureResponse;
     }
 
@@ -48,27 +46,21 @@ public class DatabaseService implements DatabaseServiceInterface {
     public CompletableFuture<DatabaseResponse> updateField(String path, String field, Object value) {
         CompletableFuture<DatabaseResponse> futureResponse = new CompletableFuture<>();
 
-        this.db.document(path).update(FieldPath.of(field), value).addOnCompleteListener(task -> {
-            try {
-                futureResponse.complete(new DatabaseResponse(task.isComplete() && task.isSuccessful(), task.getResult(), task.getException()));
-            } catch (Exception e){
-                Log.d(DEBUG_TAG, "Unable to update field from DB", task.getException());
-                futureResponse.complete(new DatabaseResponse(false, null, e));
-            }
-        });
+        this.db.document(path).update(FieldPath.of(field), value).addOnCompleteListener(task -> onTaskComplete(task, futureResponse));
+
         return futureResponse;
     }
 
     @Override
     public CompletableFuture<DatabaseResponse> getField(String path, String field) {
         return getData(path).thenApply(task -> {
-           if(task.isSuccessful()){
-               Object obj = ((Map<String, Object>) task.getResult()).get(field);
-               return new DatabaseResponse(true, obj, task.getException());
-           } else {
-               Log.d(DEBUG_TAG, "Unable to load field from DB", task.getException());
-               return task;
-           }
+            if(task.isSuccessful()){
+                Object obj = ((Map<String, Object>) task.getResult()).get(field);
+                return new DatabaseResponse(true, obj, task.getException());
+            } else {
+                Log.d(DEBUG_TAG, "Unable to load field from DB", task.getException());
+                return task;
+            }
         });
     }
 
@@ -76,14 +68,7 @@ public class DatabaseService implements DatabaseServiceInterface {
     public CompletableFuture<DatabaseResponse> getData(String path) {
         CompletableFuture<DatabaseResponse> futureResponse = new CompletableFuture<>();
 
-        this.db.document(path).get().addOnCompleteListener(task -> {
-            try {
-                futureResponse.complete(new DatabaseResponse(task.isSuccessful(), task.getResult().getData(), task.getException()));
-            } catch (Exception e){
-                Log.d(DEBUG_TAG, "Unable to load data from DB", task.getException());
-                futureResponse.complete(new DatabaseResponse(false, null, e));
-            }
-        });
+        this.db.document(path).get().addOnCompleteListener(task -> onTaskComplete(task, futureResponse));
 
         return futureResponse;
     }
@@ -91,14 +76,7 @@ public class DatabaseService implements DatabaseServiceInterface {
     public CompletableFuture<DatabaseResponse> delete(String path){
         CompletableFuture<DatabaseResponse> futureResponse = new CompletableFuture<>();
 
-        this.db.document(path).delete().addOnCompleteListener(task -> {
-            try {
-                futureResponse.complete(new DatabaseResponse(true, task.getResult(), task.getException()));
-            } catch (Exception e){
-                Log.d(DEBUG_TAG, "Unable to load data from DB", task.getException());
-                futureResponse.complete(new DatabaseResponse(false, null, e));
-            }
-        });
+        this.db.document(path).delete().addOnCompleteListener(task -> onTaskComplete(task, futureResponse));
 
         return futureResponse;
     }
@@ -106,4 +84,20 @@ public class DatabaseService implements DatabaseServiceInterface {
     public CompletableFuture<DatabaseResponse> updateArrayField(String path, String arrayField, Object value){
         return this.updateField(path, arrayField, FieldValue.arrayUnion(value));
     }
+
+    protected void onTaskComplete(Task task, CompletableFuture<DatabaseResponse> futureResponse){
+        try {
+            if(task.getResult() instanceof DocumentSnapshot){
+                futureResponse.complete(new DatabaseResponse(task.isSuccessful(), ((DocumentSnapshot) task.getResult()).getData(), task.getException()));
+            } else {
+                futureResponse.complete(new DatabaseResponse(task.isSuccessful(), task.getResult(), task.getException()));
+            }
+        } catch (Exception e){
+            futureResponse.complete(new DatabaseResponse(task.isSuccessful(), null, task.getException()));
+                /*
+                    TODO: Match exception to check if user exists or not
+                */
+        }
+    }
 }
+
