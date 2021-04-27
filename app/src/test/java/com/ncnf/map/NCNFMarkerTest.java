fbf9fe3d2eb144e60008e386d06247279e509dba
Beltan <beltan.michel@epfl.ch>
2021-04-27 14:51:07 +0200

    Fixed Markers Test

--- app/src/test/java/com/ncnf/map/NCNFMarkerTest.java
+++ app/src/test/java/com/ncnf/map/NCNFMarkerTest.java
@@ -7,18 +7,19 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class NCNFMarkerTest {
 
     @Test
     public void testMarkers(){
         LatLng position = new LatLng(46f, 32f);
-        NCNFMarker marker = new NCNFMarker(46f, 32f, "TestTitle", "TestSnippet", new ArrayList<>(), NCNFMarker.TYPE.EVENT);
+        NCNFMarker marker = new NCNFMarker(46f, 32f, "TestTitle", "TestSnippet", new ArrayList<>(), true);
 
         assertEquals(position, marker.getPosition());
         assertEquals("TestTitle", marker.getTitle());
         assertEquals("TestSnippet", marker.getSnippet());
         assertEquals(new ArrayList<>(), marker.getEventList());
-        assertEquals(NCNFMarker.TYPE.EVENT, marker.getType());
+        assertTrue(marker.isEvent());
     }
 }
