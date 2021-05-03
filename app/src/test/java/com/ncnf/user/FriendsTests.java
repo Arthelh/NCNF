fcf4649b525f5b08f6ffe40bab8cf1f225e83455
Keran Kocher <kocher.ke@gmail.com>
2021-05-03 18:15:22 +0200

    Adapt the Friends class to the new database API

--- app/src/test/java/com/ncnf/user/FriendsTests.java
+++ app/src/test/java/com/ncnf/user/FriendsTests.java
@@ -1,194 +1,169 @@
-//package com.ncnf.user;
-//
-//import com.ncnf.database.DatabaseResponse;
-//import com.ncnf.database.DatabaseService;
-//
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.mockito.Mockito;
-//
-//import java.util.Arrays;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.concurrent.CompletableFuture;
-//import java.util.concurrent.ExecutionException;
-//
-//import static com.ncnf.Utils.AWAITING_REQUESTS_KEY;
-//import static com.ncnf.Utils.FRIENDS_KEY;
-//import static com.ncnf.Utils.PENDING_REQUESTS_KEY;
-//import static com.ncnf.Utils.USERS_COLLECTION_KEY;
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
-//import static org.mockito.Matchers.any;
-//import static org.mockito.Matchers.anyList;
-//import static org.mockito.Matchers.anyObject;
-//import static org.mockito.Matchers.anyString;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//public class FriendsTests {
-//
-//    private final static String uuid = "my_uuid";
-//    private final static String other_uuid = "other_uuid";
-//
-//    private final DatabaseService mockDatabase = Mockito.mock(DatabaseService.class);
-//    private final Friends friends = new Friends(mockDatabase, uuid);
-//
-//    private List<String> ids;
-//    private HashMap<String, String> users;
-//
-//    private <T> CompletableFuture<DatabaseResponse<T>> successFuture(T result) {
-//        return CompletableFuture.completedFuture(new DatabaseResponse<T>(true, result, null));
-//    }
-//
-//    private <T> CompletableFuture<DatabaseResponse<T>> failedFuture(T result) {
-//        return CompletableFuture.completedFuture(new DatabaseResponse<T>(false, result, null));
-//    }
-//
-//    @Before
-//    public void setup() {
-//        ids = Arrays.asList("u1");
-//        users = new HashMap<>();
-//        users.put("uuid", "u1");
-//        users.put("username", "Johnny");
-//    }
-//
-//    @Test
-//    public void requestSuccessfullyUpdateTwoUsers() {
-//        when(mockDatabase.updateArrayField(anyString(), anyString(), anyObject())).thenReturn(successFuture(anyObject()));
-//
-//        CompletableFuture<DatabaseResponse<Object>> res = friends.request(other_uuid);
-//
-//        verify(mockDatabase).updateArrayField(USERS_COLLECTION_KEY + uuid, PENDING_REQUESTS_KEY, other_uuid);
-//        verify(mockDatabase).updateArrayField(USERS_COLLECTION_KEY + other_uuid, AWAITING_REQUESTS_KEY, uuid);
-//
-//        try {
-//            assertTrue(res.get().isSuccessful());
-//        } catch (ExecutionException | InterruptedException e) {
-//            Assert.fail("The future did not complete correctly !");
-//        }
-//    }
-//
-//    @Test
-//    public void requestFailsToUpdateTwoUsers() {
-//        when(mockDatabase.updateArrayField(anyString(), anyString(), anyObject())).thenReturn(failedFuture(null));
-//
-//        CompletableFuture<DatabaseResponse<Object>> res = friends.request(other_uuid);
-//
-//        try {
-//            assertFalse(res.get().isSuccessful());
-//        } catch (ExecutionException | InterruptedException e) {
-//            Assert.fail("The future did not complete correctly !");
-//        }
-//    }
-//
-//    @Test
-//    public void awaitingRequestsIsSuccessful() {
-//        when(mockDatabase.getField(anyString(), anyString(), any())).thenReturn(successFuture(ids));
-//        when(mockDatabase.whereEqualTo(anyString(), anyString(), anyList(), any())).thenReturn(successFuture(users));
-//
-//        CompletableFuture<DatabaseResponse<Object>> res = friends.awaitingRequests();
-//
-//        verify(mockDatabase).getField(USERS_COLLECTION_KEY + uuid, AWAITING_REQUESTS_KEY, any());
-//        verify(mockDatabase).whereEqualTo(USERS_COLLECTION_KEY, "uuid", ids, any());
-//
-//        try {
-//            assertTrue(res.get().isSuccessful());
-//        } catch (ExecutionException | InterruptedException e) {
-//            Assert.fail("The future did not complete correctly !");
-//        }
-//    }
-//
-//    @Test
-//    public void awaitingRequestsFailsWithTheFirstRequest() {
-//        when(mockDatabase.getField(anyString(), anyString(), any())).thenReturn(failedFuture(anyObject()));
-//
-//        CompletableFuture<DatabaseResponse<Object>> res = friends.awaitingRequests();
-//
-//        try {
-//            assertFalse(res.get().isSuccessful());
-//        } catch (ExecutionException | InterruptedException e) {
-//            Assert.fail("The future did not complete correctly !");
-//        }
-//    }
-//
-//    @Test
-//    public void awaitingRequestsFailsWithTheSecondRequest() {
-//        when(mockDatabase.getField(anyString(), anyString(), any())).thenReturn(successFuture(ids));
-//        when(mockDatabase.whereEqualTo(anyString(), anyString(), anyList(), any())).thenReturn(failedFuture(users));
-//
-//        CompletableFuture<DatabaseResponse<Object>> res = friends.awaitingRequests();
-//
-//        try {
-//            assertFalse(res.get().isSuccessful());
-//        } catch (ExecutionException | InterruptedException e) {
-//            Assert.fail("The future did not complete correctly !");
-//        }
-//    }
-//
-//    @Test
-//    public void pendingRequestsIsSuccessful() {
-//        when(mockDatabase.getField(anyString(), anyString(), any())).thenReturn(successFuture(ids));
-//        when(mockDatabase.whereEqualTo(anyString(), anyString(), anyList(), any())).thenReturn(successFuture(users));
-//
-//        CompletableFuture<DatabaseResponse<Object>> res = friends.pendingRequests();
-//
-//        verify(mockDatabase).getField(USERS_COLLECTION_KEY + uuid, PENDING_REQUESTS_KEY, any());
-//        verify(mockDatabase).whereEqualTo(USERS_COLLECTION_KEY, "uuid", ids, any());
-//
-//        try {
-//            assertTrue(res.get().isSuccessful());
-//        } catch (ExecutionException | InterruptedException e) {
-//            Assert.fail("The future did not complete correctly !");
-//        }
-//    }
-//
-//    @Test
-//    public void pendingRequestsFails() {
-//        when(mockDatabase.getField(anyString(), anyString(), any())).thenReturn(failedFuture(ids));
-//
-//        CompletableFuture<DatabaseResponse<Object>> res = friends.pendingRequests();
-//
-//        try {
-//            assertFalse(res.get().isSuccessful());
-//        } catch (ExecutionException | InterruptedException e) {
-//            Assert.fail("The future did not complete correctly !");
-//        }
-//    }
-//
-//
-//    @Test
-//    public void updateRequestIsSuccessful() {
-//        when(mockDatabase.removeArrayField(anyString(), anyString(), anyString())).thenReturn(successFuture(null));
-//        when(mockDatabase.updateArrayField(anyString(), anyString(), anyString())).thenReturn(successFuture(null));
-//
-//        CompletableFuture<DatabaseResponse<Object>> res = friends.updateRequest(true, other_uuid);
-//
-//        verify(mockDatabase).removeArrayField(USERS_COLLECTION_KEY + uuid, AWAITING_REQUESTS_KEY, other_uuid);
-//        verify(mockDatabase).removeArrayField(USERS_COLLECTION_KEY + other_uuid, PENDING_REQUESTS_KEY, uuid);
-//        verify(mockDatabase).updateArrayField(USERS_COLLECTION_KEY + uuid, FRIENDS_KEY, other_uuid);
-//        verify(mockDatabase).updateArrayField(USERS_COLLECTION_KEY + other_uuid, FRIENDS_KEY, uuid);
-//
-//        try {
-//            assertTrue(res.get().isSuccessful());
-//        } catch (ExecutionException | InterruptedException e) {
-//            Assert.fail("The future did not complete correctly !");
-//        }
-//    }
-//
-//    @Test
-//    public void updateRequestFails() {
-//        when(mockDatabase.removeArrayField(anyString(), anyString(), anyString())).thenReturn(failedFuture(null));
-//        when(mockDatabase.updateArrayField(anyString(), anyString(), anyString())).thenReturn(failedFuture(null));
-//
-//        CompletableFuture<DatabaseResponse<Object>> res = friends.updateRequest(true, other_uuid);
-//
-//        try {
-//            assertFalse(res.get().isSuccessful());
-//        } catch (ExecutionException | InterruptedException e) {
-//            Assert.fail("The future did not complete correctly !");
-//        }
-//    }
-//
-//}
+package com.ncnf.user;
+
+import com.ncnf.database.DatabaseService;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static com.ncnf.Utils.AWAITING_REQUESTS_KEY;
+import static com.ncnf.Utils.FRIENDS_KEY;
+import static com.ncnf.Utils.PENDING_REQUESTS_KEY;
+import static com.ncnf.Utils.USERS_COLLECTION_KEY;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class FriendsTests {
+
+    private final static String uuid = "my_uuid";
+    private final static String other_uuid = "other_uuid";
+
+    private final DatabaseService mockDatabase = Mockito.mock(DatabaseService.class);
+    private final Friends friends = new Friends(mockDatabase, uuid);
+
+    private List<User> users;
+
+    @Before
+    public void setup() {
+        User u1 = new User(mockDatabase, "u1", "John", "john@bar.com","John",  "Smith", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), null, false);
+        User u2 = new User(mockDatabase, "u2", "Albert", "albert@bar.com","Albert",  "Dupont", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), null, false);
+        users = Arrays.asList(u1, u2);
+    }
+
+    @Test
+    public void requestSuccessfullyUpdateTwoUsers() {
+        when(mockDatabase.updateArrayField(anyString(), anyString(), anyObject())).thenReturn(CompletableFuture.completedFuture(true));
+
+        CompletableFuture<Boolean> res = friends.request(other_uuid);
+
+        verify(mockDatabase).updateArrayField(USERS_COLLECTION_KEY + uuid, PENDING_REQUESTS_KEY, other_uuid);
+        verify(mockDatabase).updateArrayField(USERS_COLLECTION_KEY + other_uuid, AWAITING_REQUESTS_KEY, uuid);
+
+        try {
+            assertTrue(res.get());
+        } catch (ExecutionException | InterruptedException e) {
+            Assert.fail("The future did not complete correctly !");
+        }
+    }
+
+    @Test
+    public void requestFailsToUpdateTwoUsers() {
+        when(mockDatabase.updateArrayField(anyString(), anyString(), anyObject())).thenReturn(CompletableFuture.completedFuture(false));
+
+        CompletableFuture<Boolean> res = friends.request(other_uuid);
+
+        try {
+            assertFalse(res.get());
+        } catch (ExecutionException | InterruptedException e) {
+            Assert.fail("The future did not complete correctly !");
+        }
+    }
+
+    @Test
+    public void awaitingRequestsIsSuccessful() {
+        CompletableFuture<List<User>> future = CompletableFuture.completedFuture(users);
+        when(mockDatabase.whereArrayContains(anyString(), anyString(), anyObject(), eq(User.class))).thenReturn(future);
+
+        CompletableFuture<List<User>> res = friends.awaitingRequests();
+
+        verify(mockDatabase).whereArrayContains(USERS_COLLECTION_KEY, PENDING_REQUESTS_KEY, uuid, User.class);
+
+        try {
+            assertThat(res.get(), is(users));
+        } catch (ExecutionException | InterruptedException e) {
+            Assert.fail("The future did not complete correctly !");
+        }
+    }
+
+
+    @Test
+    public void pendingRequestsIsSuccessful() {
+        CompletableFuture<List<User>> future = CompletableFuture.completedFuture(users);
+        when(mockDatabase.whereArrayContains(anyString(), anyString(), anyObject(), eq(User.class))).thenReturn(future);
+
+        CompletableFuture<List<User>> res = friends.pendingRequests();
+
+        verify(mockDatabase).whereArrayContains(USERS_COLLECTION_KEY, AWAITING_REQUESTS_KEY, uuid, User.class);
+
+        try {
+            assertThat(res.get(), is(users));
+        } catch (ExecutionException | InterruptedException e) {
+            Assert.fail("The future did not complete correctly !");
+        }
+    }
+
+    @Test
+    public void acceptRequestIsSuccessful() {
+        when(mockDatabase.removeArrayField(anyString(), anyString(), anyString())).thenReturn(CompletableFuture.completedFuture(true));
+        when(mockDatabase.updateArrayField(anyString(), anyString(), anyString())).thenReturn(CompletableFuture.completedFuture(true));
+
+        CompletableFuture<Boolean> res = friends.updateRequest(true, other_uuid);
+
+        verify(mockDatabase).removeArrayField(USERS_COLLECTION_KEY + uuid, AWAITING_REQUESTS_KEY, other_uuid);
+        verify(mockDatabase).removeArrayField(USERS_COLLECTION_KEY + other_uuid, PENDING_REQUESTS_KEY, uuid);
+        verify(mockDatabase).updateArrayField(USERS_COLLECTION_KEY + uuid, FRIENDS_KEY, other_uuid);
+        verify(mockDatabase).updateArrayField(USERS_COLLECTION_KEY + other_uuid, FRIENDS_KEY, uuid);
+
+        try {
+            assertTrue(res.get());
+        } catch (ExecutionException | InterruptedException e) {
+            Assert.fail("The future did not complete correctly !");
+        }
+    }
+
+    @Test
+    public void acceptRequestFails() {
+        when(mockDatabase.removeArrayField(anyString(), anyString(), anyString())).thenReturn(CompletableFuture.completedFuture(false));
+        when(mockDatabase.updateArrayField(anyString(), anyString(), anyString())).thenReturn(CompletableFuture.completedFuture(false));
+
+        CompletableFuture<Boolean> res = friends.updateRequest(true, other_uuid);
+
+        try {
+            assertFalse(res.get());
+        } catch (ExecutionException | InterruptedException e) {
+            Assert.fail("The future did not complete correctly !");
+        }
+    }
+
+    @Test
+    public void declineRequestIsSuccessful() {
+        when(mockDatabase.removeArrayField(anyString(), anyString(), anyString())).thenReturn(CompletableFuture.completedFuture(true));
+
+        CompletableFuture<Boolean> res = friends.updateRequest(false, other_uuid);
+
+        try {
+            assertTrue(res.get());
+        } catch (ExecutionException | InterruptedException e) {
+            Assert.fail("The future did not complete correctly !");
+        }
+    }
+
+    @Test
+    public void getFriendsIsSuccessful() {
+        CompletableFuture<List<User>> future = CompletableFuture.completedFuture(users);
+        when(mockDatabase.whereArrayContains(anyString(), anyString(), anyString(), eq(User.class))).thenReturn(future);
+
+        CompletableFuture<List<User>> res = friends.getFriends();
+        
+        try {
+            assertThat(res.get(), is(users));
+        } catch (ExecutionException | InterruptedException e) {
+            Assert.fail("The future did not complete correctly !");
+        }
+    }
+
+}
