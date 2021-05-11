1bda48f192502cd40bd8bfbf03f4231cd12f3b27
Jérémie Frei <jeremie.frei@epfl.ch>
2021-05-11 17:47:59 +0200

    Update FriendsActivity.java

--- app/src/main/java/com/ncnf/friends/ui/FriendsActivity.java
+++ app/src/main/java/com/ncnf/friends/ui/FriendsActivity.java
@@ -2,7 +2,6 @@ package com.ncnf.friends.ui;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,63 +9,76 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
-import com.google.android.material.button.MaterialButton;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.ncnf.R;
-import com.ncnf.user.User;
-import com.ncnf.views.fragments.PublicProfileFragment;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class FriendsActivity extends AppCompatActivity {
 
-    private final FragmentManager fragmentManager = getSupportFragmentManager();
-    private Fragment activeFragment;
-    private Fragment friendsFragment;
-    private Fragment addFriendFragment;
-    private Fragment userProfileFragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
 
-        this.friendsFragment = new FriendsFragment();
-        this.addFriendFragment = new AddFriendFragment();
-        this.activeFragment = this.friendsFragment;
+        // Setup fragment selector
+        FragmentSelector fragmentSelector = new FragmentSelector(this);
+        ViewPager2 viewPager = findViewById(R.id.friends_view_pager);
+        viewPager.setAdapter(fragmentSelector);
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                fragmentSelector.notifyDataSetChanged();
+            }
+        });
 
-        fragmentManager.beginTransaction().add(R.id.friends_fragment_container, friendsFragment).hide(friendsFragment).commit();
-        fragmentManager.beginTransaction().add(R.id.friends_fragment_container, addFriendFragment).hide(addFriendFragment).commit();
 
-        findViewById(R.id.friends_switch_button).setOnClickListener(this::onAddButtonClicked);
-
-        showActiveFragment();
+        TabLayout tabs = findViewById(R.id.friends_tabs);
+        new TabLayoutMediator(tabs, viewPager,
+                (tab, position) -> tab.setText(fragmentSelector.getPageTitle(position))
+        ).attach();
     }
 
-    private void onAddButtonClicked(View view) {
-        MaterialButton addButton = (MaterialButton) view;
+    private static class FragmentSelector extends FragmentStateAdapter {
 
-        if(activeFragment instanceof FriendsFragment){
-            addButton.setIcon(getDrawable(R.drawable.ic_baseline_format_list_bulleted_24));
-            fragmentManager.beginTransaction().hide(activeFragment).show(addFriendFragment).commit();
-            this.activeFragment = addFriendFragment;
-        } else if(activeFragment instanceof AddFriendFragment) {
-            addButton.setIcon(getDrawable(R.drawable.ic_baseline_add_24));
-            fragmentManager.beginTransaction().hide(activeFragment).show(friendsFragment).commit();
-            this.activeFragment = friendsFragment;
+        private final String[] TAB_TITLES = new String[]{"Friends", "My requests"};
+        private final int itemCount = TAB_TITLES.length;
+
+        public FragmentSelector(FragmentActivity activity) {
+            super(activity);
+        }
+
+        public CharSequence getPageTitle(int position) {
+            return TAB_TITLES[position];
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch(position) {
+                case 0: return new FriendsFragment();
+                case 1:
+                default: return new FriendsRequestsFragment();
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return itemCount;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return super.getItemId(position);
+        }
+
+        @Override
+        public boolean containsItem(long itemId) {
+            return super.containsItem(itemId);
         }
     }
-
-    protected void showPublicProfileFragment(User user){
-        Toast.makeText(this, "TEST_PROFILE_DISPLAY", Toast.LENGTH_LONG).show();
-        this.userProfileFragment = new PublicProfileFragment(user);
-        fragmentManager.beginTransaction().replace(R.id.friends_fragment_container, userProfileFragment).commit();
-    }
-
-
-    protected void showActiveFragment(){
-        fragmentManager.beginTransaction().show(activeFragment).addToBackStack("active").commit();
-    }
-
 }
