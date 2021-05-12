1818a0291e1448943d9524e2dc428730b626d612
HyPn0TiiK <david.pedrucci@epfl.ch>
2021-05-12 11:41:12 +0200

    changed event time when saving to calendar

--- app/src/main/java/com/ncnf/utilities/SaveToCalendar.java
+++ app/src/main/java/com/ncnf/utilities/SaveToCalendar.java
@@ -1,20 +1,24 @@
-package com.ncnf.socialObject;
+package com.ncnf.utilities;
 
 import android.content.Intent;
 import android.provider.CalendarContract;
+import android.view.View;
+import android.widget.Button;
 
-public class Add2Calendar {
+import com.ncnf.socialObject.SocialObject;
+
+public class SaveToCalendar {
 
     public static Intent createCalendarIntent(SocialObject s){
+
         Intent intent = new Intent(Intent.ACTION_INSERT);
         intent.setData(CalendarContract.Events.CONTENT_URI);
         intent.putExtra(CalendarContract.Events.TITLE, s.getName());
         intent.putExtra(CalendarContract.Events.EVENT_LOCATION, s.getLocation().toString());
         intent.putExtra(CalendarContract.Events.DESCRIPTION, s.getDescription());
-        intent.putExtra(CalendarContract.Events.ALL_DAY, true);
-        //TODO if hour is added
-        /*intent.putExtra(CalendarContract.Events.DTSTART, hour start )
-        intent.putExtra(CalendarContract.Events.DTEND, hour end)*/
+        //intent.putExtra(CalendarContract.Events.ALL_DAY, true);
+        intent.putExtra(CalendarContract.Events.DTSTART, s.getDate().getTime());
+        //intent.putExtra(CalendarContract.Events.DTEND, hour end)*/
         return intent;
     }
 }
