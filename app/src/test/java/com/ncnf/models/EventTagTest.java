796c05ea9f47978e180f00b2ef112dd977c871fe
Jérémie Frei <jeremie.frei@epfl.ch>
2021-05-22 14:22:17 +0200

    Update EventTagTest.java

--- app/src/test/java/com/ncnf/models/EventTagTest.java
+++ app/src/test/java/com/ncnf/models/EventTagTest.java
@@ -8,16 +8,26 @@ import static org.junit.Assert.assertEquals;
 public class EventTagTest {
 
     @Test
-    public void testTag(){
+    public void getNameWorks(){
         String emoji = "test1", name = "test2";
         EventTag eventTag = new EventTag(emoji, name);
-        assertEquals(emoji, eventTag.getEmoji());
         assertEquals(name, eventTag.getName());
 
         String new_emoji = "test3", new_name = "test4";
         eventTag.setEmoji(new_emoji);
         eventTag.setName(new_name);
-        assertEquals(new_emoji, eventTag.getEmoji());
         assertEquals(new_name, eventTag.getName());
+    }
+
+    @Test
+    public void getEmojiWorks(){
+        String emoji = "test1", name = "test2";
+        EventTag eventTag = new EventTag(emoji, name);
+        assertEquals(emoji, eventTag.getEmoji());
+
+        String new_emoji = "test3", new_name = "test4";
+        eventTag.setEmoji(new_emoji);
+        eventTag.setName(new_name);
+        assertEquals(new_emoji, eventTag.getEmoji());
     }
 }
