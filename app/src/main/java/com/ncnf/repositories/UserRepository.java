fcf002a1a88a7cd2d7878243f778da79cf56433a
Jérémie Frei <jeremie.frei@epfl.ch>
2021-05-22 16:29:55 +0200

    Update UserRepository.java

--- app/src/main/java/com/ncnf/repositories/UserRepository.java
+++ app/src/main/java/com/ncnf/repositories/UserRepository.java
@@ -9,7 +9,6 @@ import java.util.concurrent.CompletableFuture;
 
 import javax.inject.Inject;
 
-import static com.ncnf.utilities.StringCodes.LOCATION_KEY;
 import static com.ncnf.utilities.StringCodes.NOTIFICATIONS_KEY;
 import static com.ncnf.utilities.StringCodes.NOTIFICATIONS_TOKEN_KEY;
 import static com.ncnf.utilities.StringCodes.USERS_COLLECTION_KEY;
@@ -57,18 +56,41 @@ public class UserRepository {
         return this.db.whereIn(USERS_COLLECTION_KEY, UUID_KEY, uuidList, User.class);
     }
 
+    /**
+     * Updates the User's notification field
+     * @param uuid the unique identifier of the User
+     * @param isEnabled the new value for notifications
+     * @return A CompletableFuture wrapping a boolean indicating that the request was successful or not
+     */
     public CompletableFuture<Boolean> updateNotifications(String uuid, boolean isEnabled) {
         return this.db.updateField(USERS_COLLECTION_KEY + uuid, NOTIFICATIONS_KEY, isEnabled);
     }
 
+    /**
+     * Updates the User's notification token
+     * @param uuid the unique identifier of the User
+     * @param token the new token
+     * @return A CompletableFuture wrapping a boolean indicating that the request was successful or not
+     */
     public CompletableFuture<Boolean> updateNotificationsToken(String uuid, String token) {
         return this.db.updateField(USERS_COLLECTION_KEY + uuid, NOTIFICATIONS_TOKEN_KEY, token);
     }
 
+    /**
+     * Updates the User's location field
+     * @param uuid the unique identifier of the User
+     * @param location the new location as a GeoPoint
+     * @return A CompletableFuture wrapping a boolean indicating that the request was successful or not
+     */
     public CompletableFuture<Boolean> updateUserPosition(String uuid, GeoPoint location){
         return this.db.updateField(USERS_COLLECTION_KEY + uuid, USER_LOCATION_KEY, location);
     }
 
+    /**
+     * Gets the User's location field
+     * @param uuid the unique identifier of the User
+     * @return A CompletableFuture wrapping the user's location
+     */
     public CompletableFuture<GeoPoint> getUserPosition(String uuid){
         return this.db.getField(USERS_COLLECTION_KEY + uuid, USER_LOCATION_KEY);
     }
