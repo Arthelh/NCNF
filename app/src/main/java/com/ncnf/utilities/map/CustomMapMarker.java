595819796a2d871a76706e58b9618994366dfb2a
Beltan <beltan.michel@epfl.ch>
2021-05-24 11:19:35 +0200

    Modified a couple things in CustomMapMarker as requested

--- app/src/main/java/com/ncnf/utilities/map/CustomMapMarker.java
+++ app/src/main/java/com/ncnf/utilities/map/CustomMapMarker.java
@@ -8,9 +8,14 @@ import com.google.maps.android.clustering.ClusterItem;
 import com.ncnf.models.Event;
 import com.ncnf.models.Organization;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A CustomMapMarker that holds the necessary information to display the info windows when clicked
+ * and to lead to the linked pages. These Markers can be clustered together
+ */
 public class CustomMapMarker implements ClusterItem {
 
     private final LatLng position;
@@ -20,10 +25,29 @@ public class CustomMapMarker implements ClusterItem {
     private final Organization org;
     private final boolean isEvent;
 
+    /**
+     * Creates a new CustomMapMarker which can be displayed on the map and clustered using a ClusterManager
+     * @param lat The latitude of the anchor point in float
+     * @param lon The longitude of the anchor point in float
+     * @param title The title to show on the info window
+     * @param snippet The snippet to show on the info window, this is the second text that appears below the title
+     * @param eventList The list of events this marker represents, must have isEvent true
+     * @param org The organization this marker represents, must have isEvent false
+     * @param isEvent True if this organization represents an event, false otherwise
+     */
     public CustomMapMarker(float lat, float lon, String title, String snippet, List<Event> eventList, Organization org, boolean isEvent){
         this(new LatLng(lat, lon), title, snippet, eventList, org, isEvent);
     }
 
+    /**
+     * Creates a new CustomMapMarker which can be displayed on the map and clustered.
+     * @param position The anchor position of the marker on the map as a LatLng
+     * @param title The title to show on the info window
+     * @param snippet The snippet to show on the info window, this is the second text that appears below the title
+     * @param eventList The list of events this marker represents, must have isEvent true
+     * @param org The organization this marker represents, must have isEvent false
+     * @param isEvent True if this organization represents an event, false otherwise
+     */
     public CustomMapMarker(LatLng position, String title, String snippet, List<Event> eventList, Organization org, boolean isEvent){
         this.position = position;
         this.title = title;
@@ -34,7 +58,7 @@ public class CustomMapMarker implements ClusterItem {
             this.eventList = eventList;
             this.org = null;
         } else {
-            this.eventList = null;
+            this.eventList = new ArrayList<>();
             this.org = org;
         }
     }
