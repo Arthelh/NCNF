b2462e27d505b8480fd2044759cf6c717e0ca66c
Beltan <beltan.michel@epfl.ch>
2021-06-06 14:41:11 +0200

    Unskipped tests to see if Cirrus accepts them

--- app/src/androidTest/java/com/ncnf/views/fragments/map/MapFragmentTest.java
+++ app/src/androidTest/java/com/ncnf/views/fragments/map/MapFragmentTest.java
@@ -222,7 +222,7 @@ public final class MapFragmentTest {
 
     }
 
-    @Ignore
+    //@Ignore
     @Test
     public final void testInfoWindow(){
         UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
